9641bbc1c0640f40fa281a4c0dfd4c4ead31d8dd
Andrey <kouznts@ya.ru>
2020-06-03 18:32:49 +0400

    create can paginate test

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,10 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Webapp.Controllers;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SportsStore.UnitTests
 {
@@ -7,8 +12,27 @@ namespace SportsStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products)
+                .Returns(new Product[]
+                {
+                    new Product {ProductId = 1, Name = "P1"},
+                    new Product {ProductId = 2, Name = "P2"},
+                    new Product {ProductId = 3, Name = "P3"},
+                    new Product {ProductId = 4, Name = "P4"},
+                    new Product {ProductId = 5, Name = "P5"},
+                });
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+
+            Product[] products = result.ToArray();
+            Assert.IsTrue(products.Length == 2);
+            Assert.AreEqual(products[0].Name, "P4");
+            Assert.AreEqual(products[1].Name, "P5");
         }
     }
 }
