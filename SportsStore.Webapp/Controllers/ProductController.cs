cf9bdfa2e00426e447a3d9ca41495ddfa1da0803
Andrey <kouznts@ya.ru>
2020-06-03 18:32:22 +0400

    create pagination

--- SportsStore.Webapp/Controllers/ProductController.cs
+++ SportsStore.Webapp/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using SportsStore.Domain.Abstract;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace SportsStore.Webapp.Controllers
@@ -6,15 +7,19 @@ namespace SportsStore.Webapp.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repository)
         {
             this.repository = repository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
         }
     }
 }
