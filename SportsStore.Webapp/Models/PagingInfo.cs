8faf1a8ed303f872cb39945dc123f0c03a2481f7
Andrey <kouznts@ya.ru>
2020-06-03 18:44:51 +0400

    create paging info class

---
+++ SportsStore.Webapp/Models/PagingInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SportsStore.Webapp.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+
+        public int ItemsPerPage { get; set; }
+
+        public int CurrPage { get; set; }
+
+        public int TotalPages
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+            }
+        }
+    }
+}
