a2970ba18baeb0f24ebb122ec596b092697ad273
Andrey <kouznts@ya.ru>
2020-06-03 09:44:59 +0400

    create Ninject dependency resolver

---
+++ SportsStore.Webapp/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+using System.Web.Mvc;
+using Ninject;
+
+namespace SportsStore.Webapp.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            this.kernel = kernel;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+
+        }
+    }
+}
