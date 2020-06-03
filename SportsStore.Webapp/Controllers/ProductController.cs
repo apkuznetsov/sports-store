7be9374e16531e34d2291f10d51d4c6e70e1c490
Andrey <kouznts@ya.ru>
2020-06-03 10:12:16 +0400

    create product controller

---
+++ SportsStore.Webapp/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using SportsStore.Domain.Abstract;
+using System.Web.Mvc;
+
+namespace SportsStore.Webapp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
