35105c1deb80c73f21eecd9bcb7df85b0e391425
Andrey <kouznts@ya.ru>
2020-06-04 17:37:10 +0400

    fix all usings

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -6,7 +6,6 @@ using SportsStore.Webapp.Controllers;
 using SportsStore.Webapp.HtmlHelpers;
 using SportsStore.Webapp.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
