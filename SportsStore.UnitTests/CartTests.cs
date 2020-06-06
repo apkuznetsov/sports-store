c68b7c4df52e318bb17d0ac041aa5bd7ca30990d
Andrey <kouznts@ya.ru>
2020-06-06 12:00:35 +0400

    create can add new lines test

---
+++ SportsStore.UnitTests/CartTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+using System.Linq;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+            Product p1 = new Product { ProductId = 1, Name = "P1" };
+            Product p2 = new Product { ProductId = 2, Name = "P2" };
+            Cart target = new Cart();
+
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            CartLine[] results = target.Lines.ToArray();
+
+            Assert.AreEqual(results.Length, 2);
+            Assert.AreEqual(results[0].Product, p1);
+            Assert.AreEqual(results[1].Product, p2);
+        }
+    }
+}
