8d5cab97b2e703e3d36dcb9a4c3e4635ba3d8f64
Andrey <kouznts@ya.ru>
2020-06-06 13:16:58 +0400

    create add to cart method

---
+++ SportsStore.Webapp/Controllers/CartController.cs
@@ -0,0 +1,43 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SportsStore.Webapp.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+    }
+}
