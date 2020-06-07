3868b1bd47202e979334707d27930199b52e475f
Andrey <kouznts@ya.ru>
2020-06-07 09:43:34 +0400

    create email settings class

---
+++ SportsStore.Domain/Concrete/EmailOrderProcessor.cs
@@ -0,0 +1,19 @@
+﻿namespace SportsStore.Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = "orders@example.com";
+        public string MailFromAddress = "sportsstore@example.com";
+        public bool UseSsl = true;
+        public string Username = "MySmtpUsername";
+        public string Password = "MySmtpPassword";
+        public string ServerName = "smpt.example.com";
+        public int ServerPort = 587;
+        public bool WriteAsFile = false;
+        public string FileLocation = @"c:\sports-store-emails";
+    }
+
+    public class EmailOrderProcessor
+    {
+    }
+}
