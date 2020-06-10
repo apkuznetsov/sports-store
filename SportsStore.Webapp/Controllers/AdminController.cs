a5e56426ee0e33bcfe4bf0e33654ca87f8c0acfa
Andrey <kouznts@ya.ru>
2020-06-10 17:55:16 +0400

    create admin controller

---
+++ SportsStore.Webapp/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using SportsStore.Domain.Abstract;
+using System.Web.Mvc;
+
+namespace SportsStore.Webapp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
