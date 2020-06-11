a40949329e6c733ae273545bd25fc945e06a80db
Andrey <kouznts@ya.ru>
2020-06-11 17:36:25 +0400

    create can login with valid credentials test

---
+++ SportsStore.UnitTests/AdminSecurityTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using SportsStore.Webapp.Controllers;
+using System.Web.Mvc;
+using SportsStore.Webapp.Infrastructure.Abstract;
+using SportsStore.Webapp.Models;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminSecurityTests
+    {
+        [TestMethod]
+        public void Can_Login_With_Valid_Credentials()
+        {
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("admin", "secret")).Returns(true);
+            LoginViewModel model = new LoginViewModel
+            {
+                UserName = "admin",
+                Password = "secret"
+            };
+            AccountController target = new AccountController(mock.Object);
+
+            ActionResult result = target.Login(model, "/MyURL");
+
+            Assert.IsInstanceOfType(result, typeof(RedirectResult));
+            Assert.AreEqual("/MyURL", ((RedirectResult)result).Url);
+        }
+    }
+}
