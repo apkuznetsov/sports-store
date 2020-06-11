0cef0e2b512512604c228f8006b261f8b881c57b
Andrey <kouznts@ya.ru>
2020-06-11 17:48:57 +0400

    create cannot login with invalid credentials test

--- SportsStore.UnitTests/AdminSecurityTests.cs
+++ SportsStore.UnitTests/AdminSecurityTests.cs
@@ -31,5 +31,23 @@ namespace SportsStore.UnitTests
             Assert.IsInstanceOfType(result, typeof(RedirectResult));
             Assert.AreEqual("/MyURL", ((RedirectResult)result).Url);
         }
+
+        [TestMethod]
+        public void Cannot_Login_With_Invalid_Credentials()
+        {
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("badUser", "badPass")).Returns(false);
+            LoginViewModel model = new LoginViewModel
+            {
+                UserName = "badUser",
+                Password = "badPass"
+            };
+            AccountController target = new AccountController(mock.Object);
+
+            ActionResult result = target.Login(model, "/MyURL");
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.IsFalse(((ViewResult)result).ViewData.ModelState.IsValid);
+        }
     }
 }
