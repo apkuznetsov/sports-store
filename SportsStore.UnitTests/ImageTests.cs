14bc5d79c594c663d02dbac1c316ad24274dc2a9
Andrey <kouznts@ya.ru>
2020-06-11 19:52:35 +0400

    create can retrieve image data method

---
+++ SportsStore.UnitTests/ImageTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Webapp.Controllers;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class ImageTests
+    {
+        [TestMethod]
+        public void Can_Retrieve_Image_Data()
+        {
+            Product product2 = new Product
+            {
+                ProductId = 2,
+                Name = "Test",
+                ImageData = new byte[] { },
+                ImageMimeType = "image/png"
+            };
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                 new Product {ProductId = 1, Name = "P1"},
+                 product2,
+                 new Product {ProductId = 3, Name = "P3"}
+            }.AsQueryable());
+            ProductController target = new ProductController(mock.Object);
+
+            ActionResult result = target.GetImage(2);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(FileResult));
+            Assert.AreEqual(product2.ImageMimeType, ((FileResult)result).ContentType);
+        }
+    }
+}
