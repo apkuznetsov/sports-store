d93d12d1209ec0d90b21404fdd36900f5c49e862
Andrey <kouznts@ya.ru>
2020-06-11 15:31:15 +0400

    create can delete valid products test

--- SportsStore.UnitTests/AdminTests.cs
+++ SportsStore.UnitTests/AdminTests.cs
@@ -94,5 +94,22 @@ namespace SportsStore.UnitTests
             mock.Verify(m => m.SaveProduct(It.IsAny<Product>()), Times.Never());
             Assert.IsInstanceOfType(result, typeof(ViewResult));
         }
+
+        [TestMethod]
+        public void Can_Delete_Valid_Products()
+        {
+            Product product2 = new Product { ProductId = 2, Name = "Test" };
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                 new Product {ProductId = 1, Name = "P1"},
+                 product2,
+                 new Product {ProductId = 3, Name = "P3"},
+                 });
+            AdminController target = new AdminController(mock.Object);
+
+            target.Delete(product2.ProductId);
+
+            mock.Verify(m => m.DeleteProduct(product2.ProductId));
+        }
     }
 }
